390cc354d6ec102ec2abf862c6946cc265ea5666
gabriel ferri <107404209+ferrigb@users.noreply.github.com>
2024-04-15 09:04:11 -0300

    alterei css

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function verificarTime() {
+  
+    var continuarPerguntando = true;
+  
+    while (continuarPerguntando) {
+      
+      var respostaTime = prompt("Qual o melhor time?");
+  
+      
+      if (respostaTime.toLowerCase() === "Palmeiras") {
+        alert("Isso mesmo! O Palmeiras é o melhor time!");
+        continuarPerguntando = false; // Sai do loop após a resposta correta
+      } else {
+        alert("Não é isso! Tente novamente.");
+      }
+    }
+  }
+  
+  // Chamada da função para iniciar o processo
+  verificarTime();
+  
+    
